6cfaf248f74cad800d85ba56da0a544f396eb1b3
adubimalaca-del <adubimalaca@gmail.com>
2026-04-14 20:21:42 -0300

    Add files via upload

---
+++ exercicio5.js
@@ -0,0 +1,11 @@
+let jogo1 = 2;
+let jogo2 = 1;
+let jogo3 = 3;
+let jogo4 = 0;
+let jogo5 = 4;
+
+let totalGols = jogo1 + jogo2 + jogo3 + jogo4 + jogo5;
+let mediaGols = totalGols / 5;
+
+console.log("Total de gols:", totalGols);
+console.log("Média de gols por partida:", mediaGols);
